112a97bb2c44cf67a7f62319556e69dafa0c438d
Chantel Wetzel <chantel.wetzel@noaa.gov>
2020-08-12 06:37:04 -0700

    fix: update example script

--- examples/Yellowtail.R
+++ examples/Yellowtail.R
@@ -1,78 +1,31 @@
 dont source
 
 rm(list=ls(all=TRUE))
-setwd("C:/Assessments/ObserverDatabase/2017/")
-#source("../ExtractR/Rcode/functions/Functions.R")
-files <- dir("Rcode/functions")
-for(i in 1:length(files)) {
-	source(file.path("Rcode/functions", files[i]))
-}
+
+load(nwfscDiscard)
 
 species = "Yellowtail Rockfish"
 species.save = "Yellowtail"
 
 # Load all the date to evaluate how many observations for 
-# California Scorpionfish
 
-load("extractedData/ob.pre2011.Rdat") #object named ob
-ob.all <- ob
-load("extractedData/ob.2011.Rdat") #object named ob
-ob.all <- rbind(ob.all,ob)
-load("extractedData/ob.2012.Rdat") #object named ob
-ob.all <- rbind(ob.all,ob)
-load("extractedData/ob.2013.Rdat") #object named ob
-ob.all <- rbind(ob.all,ob)
-load("extractedData/ob.2014.Rdat") #object named ob
-ob.all <- rbind(ob.all,ob)
-load("extractedData/ob.2015.Rdat") #object named ob
-ob.all <- rbind(ob.all,ob)
+load(" load the data ") 
+ob <- OBCatch
 
-ob <- ob.all
-rm(ob.all)
 
+# Do som examinations of the data for the species by year
 table(ob$year, ob$COMMON_NAME == species)
 
-#        FALSE   TRUE
-#   2002  68346    323
-#   2003  59584    166
-#   2004  96026    394
-#   2005  95960    454
-#   2006  72066    275
-#   2007  77948    207
-#   2008  90160    190
-#   2009 103317    184
-#   2010  85494    248
-#   2011 227928    720
-#   2012 205634    874
-#   2013 210524    694
-#   2014 195173    781
-#   2015 196965    886
 
-
+# How much data is there by state?
 table(ob$r_state, ob$COMMON_NAME == species)
-#      FALSE    TRUE
-#  CA  518918     898
-#  OR 1047238    3941
-#  WA  218969    1557
 
-
+# Subset the data to only work with the single species.
 ind = which(ob$COMMON_NAME == species)
 sub.ob = ob[ind,]
 table(sub.ob$year)
 
 table(sub.ob$sector, sub.ob$gear2)
-#                          Bottom Trawl Hook & Line Midwater Trawl Shrimp Trawl
-#  Catch Shares                    2534          65            166            0
-#  LE CA Halibut                      4           0              0            0
-#  LE Fixed Gear DTL                  0           4              0            0
-#  Limited Entry Sablefish            0         348              0            0
-#  Limited Entry Trawl             1403           0             14            0
-#  Midwater Hake                      0           0             17            0
-#  Midwater Rockfish                  0           0            117            0
-#  Nearshore                          0        1384              0            0
-#  OA Fixed Gear                      0          15              0            0
-#  Pink Shrimp                        0           0              0          190
-#  Shoreside Hake                     0           0            135            0
 
 
 aggregate(DIS ~ year + gear2 + sector, FUN = sum, sub.ob)
@@ -102,53 +55,18 @@ run.name = "GearArea"
 #######################################################################################################################
 ### check for confidentiality using all observations
 out <- NULL
-load("extractedData/ob.pre2011.Rdat") #object named ob
+
 ob$area = ob$AREA
 out$'pre2011' <- checkConfidentiality(dat = ob, 
 				 colnms = c("area", 'gear2'),
 				 colLevs= list(areaLevels, gearLevels), 
 				 strNms = list(areaNames, gearNames))
 
-load("extractedData/ob.2011.Rdat") #object named ob
-ob$area = ob$AREA
-out$'2011' <- checkConfidentiality(dat = ob, 
-				 colnms = c("area", 'gear2'),
-				 colLevs= list(areaLevels, gearLevels), 
-				 strNms = list(areaNames, gearNames))
-
-load("extractedData/ob.2012.Rdat") #object named ob
-ob$area = ob$AREA
-out$'2012' <- checkConfidentiality(dat = ob, 
-			  colnms = c("area", 'gear2'),
-			  colLevs= list(areaLevels, gearLevels), 
-			  strNms = list(areaNames, gearNames))
-
-load("extractedData/ob.2013.Rdat") #object named ob
-ob$area = ob$AREA
-out$'2013' <- checkConfidentiality(dat = ob, 
-				 colnms = c("area", 'gear2'),
-				 colLevs= list(areaLevels, gearLevels), 
-				 strNms = list(areaNames, gearNames))
-
-load("extractedData/ob.2014.Rdat") #object named ob
-ob$area = ob$AREA
-out$'2014' <- checkConfidentiality(dat = ob, 
-				 colnms = c("area", 'gear2'),
-				 colLevs= list(areaLevels, gearLevels), 
-				 strNms = list(areaNames, gearNames))
-
-load("extractedData/ob.2015.Rdat") #object named ob
-ob$area = ob$AREA
-out$'2015' <- checkConfidentiality(dat = ob, 
-				 colnms = c("area", 'gear2'),
-				 colLevs= list(areaLevels, gearLevels), 
-				 strNms = list(areaNames, gearNames))
-
-save(out,file=paste0(species.save,"/RdatFiles/allForConfidentiality_", run.name, ".Rdat"))
+save(out, file = file.name (species.save, "RdatFiles/allForConfidentiality_", run.name, ".Rdat"))
 
 
 ### Start here if already done above
-load(paste0(species.save,"/RdatFiles/allForConfidentiality_", run.name, ".Rdat")
+load(file.name(species.save, "RdatFiles/allForConfidentiality_", run.name, ".Rdat")
 
 
 
--- examples/YellowtailBootstrap.R
+++ examples/YellowtailBootstrap.R
@@ -5,36 +5,14 @@ rm(list=ls(all=TRUE))
 species = "Yellowtail Rockfish"
 save.name = "Yellowtail"
 
-setwd("C:/Assessments/ObserverDatabase")
-source("Rcode/functions/observRfunctions.R")
-source("Rcode/functions/checkConfidentiality.R")
-source("C:/Assessments/ObserverDatabase/2017/Rcode/functions/Boot_summary_fxn_jej.r")
-source("C:/Assessments/ObserverDatabase/2017/Rcode/functions/bootstrapDiscardBiomass_ach_V3.r")
-source("C:/Assessments/ObserverDatabase/2017/Rcode/functions/bootstrapDiscardRatio_ach_V3.r")
-source("C:/Assessments/ObserverDatabase/Rcode/BootstrapDiscardData.R")
+library(nwfscDiscard)
 
-source("C:/Assessments/ObserverDatabase/2017/Rcode/discardsNonCatchShares.R")
-source("C:/Assessments/ObserverDatabase/2017/Rcode/discardsCatchShares.R")
+dte = Sys.Date()
 
-dte=Sys.Date()
+setwd(" Set the working directory")
+load(" load the data ") 
 
-setwd("C:/Assessments/ObserverDatabase/2017")
-load("extractedData/ob.pre2011.Rdat") #object named ob
-ob.all <- ob
-load("extractedData/ob.2011.Rdat") #object named ob
-ob.all <- rbind(ob.all,ob)
-load("extractedData/ob.2012.Rdat") #object named ob
-ob.all <- rbind(ob.all,ob)
-load("extractedData/ob.2013.Rdat") #object named ob
-ob.all <- rbind(ob.all,ob)
-load("extractedData/ob.2014.Rdat") #object named ob
-ob.all <- rbind(ob.all,ob)
-load("extractedData/ob.2015.Rdat") #object named ob
-ob.all <- rbind(ob.all,ob)
-
-ob <- ob.all
-rm(ob.all)
-
+ob <- OBCatch
 ob$area <- ob$AREA
 
 ############################################################################################################################
@@ -44,31 +22,31 @@ ob$area <- ob$AREA
 
 gearLevels  <- list(c("Bottom Trawl", "Midwater Trawl", "Shrimp Trawl"), "Hook & Line")
 gearNames   <- c("BottomTrawl","Hook&Line")
-areaLevels <- c("SOUTH", "NORTH")  #r_state
-areaNames <- c("South4010", "North4010")
+areaLevels  <- c("SOUTH", "NORTH")  #r_state
+areaNames   <- c("South4010", "North4010")
 
 run = "GearArea"
 
 outBoot <- bootstrapDiscardData(ob, sp = species, B=1,
-	            colnms=c("area","gear2"),
-	            colnms.new=c("Area","gear3"),
-				colLevs=list(areaLevels,gearLevels),
-				stratNms=list(areaNames,gearNames),
-				bootFile= paste0(save.name, '/RdatFiles/noboot_',run,'.out'),
-				resultsFile=paste0(save.name, '/RdatFiles/noboot_dat_',run,'.out') )
+	            colnms = c("area", "gear2"),
+	            colnms.new = c("Area", "gear3"),
+				colLevs = list(areaLevels, gearLevels),
+				stratNms = list(areaNames, gearNames),
+				bootFile = file.path(save.name, 'RdatFiles', 'noboot_', run, '.out'),
+				resultsFile = paste0(save.name, 'RdatFiles', 'noboot_dat_', run, '.out') )
 
-write.csv(outBoot$ncs,paste0(save.name,'/', save.name, '_OB_DisRatios_noboot_ncs_',run,'_',dte,'.csv'),row.names=F)
-write.csv(outBoot$cs, paste0(save.name,'/', save.name, '_OB_DisRatios_noboot_cs_',run, '_',dte,'.csv'),row.names=F)
+write.csv(outBoot$ncs, file.path(save.name, paste0(save.name, '_OB_DisRatios_noboot_ncs_',run, '_', dte, '.csv') ), row.names = FALSE)
+write.csv(outBoot$cs,  file.path(save.name, paste0(save.name, '_OB_DisRatios_noboot_cs_', run, '_', dte, '.csv') ), row.names = FALSE)
 
 
-outBoot <- bootstrapDiscardData(ob,sp = species, B = 10000,
-	            colnms=c("area","gear2"),
-	            colnms.new=c("Area","gear3"),
-				colLevs=list(areaLevels,gearLevels),
-				stratNms=list(areaNames,gearNames),
-				bootFile=paste0(save.name, '/RdatFiles/boot_',run,'.out'),
-				resultsFile=paste0(save.name, '/RdatFiles/boot_dat',run,'.out') )
+outBoot <- bootstrapDiscardData(ob, sp = species, B = 10000,
+	            colnms = c("area", "gear2"),
+	            colnms.new = c("Area", "gear3"),
+				colLevs = list(areaLevels, gearLevels),
+				stratNms = list(areaNames, gearNames),
+				bootFile = file.path(save.name, 'RdatFiles', 'boot_', run, '.out'),
+				resultsFile = paste0(save.name, 'RdatFiles', 'boot_dat', run, '.out') )
 
-write.csv(outBoot$ncs,paste0(save.name, '/', save.name, '_OB_DisRatios_boot_ncs_',run,'_',dte,'.csv'),row.names=F)
-write.csv(outBoot$cs, paste0(save.name, '/', save.name, '_OB_DisRatios_boot_cs_',run,'_', dte,'.csv'),row.names=F)
+write.csv(outBoot$ncs, file.path(save.name, paste0(save.name, '_OB_DisRatios_boot_ncs_',run, '_', dte,'.csv')), row.names = FALSE)
+write.csv(outBoot$cs,  file.path(save.name, paste0(save.name, '_OB_DisRatios_boot_cs_', run, '_', dte,'.csv')), row.names = FALSE)
 
