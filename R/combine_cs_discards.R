d74cf9c584d382a1112159f0bcf0123bf19d1667
Chantel Wetzel <chantel.wetzel@noaa.gov>
2024-06-06 09:16:11 -0700

    new function to combine cs discards

---
+++ R/combine_cs_discards.R
@@ -0,0 +1,19 @@
+#' Bootstrap uncertainty and summarize WCGOP discard data
+#'
+#' @param dir Directory location to save files.
+#' @param cs_data Data object of the processed catch share discards from the OBCatch data.
+#' @param em_data Data object of the processed em catch share discards from the EMCatch data.
+#'
+#'
+#' @author Chantel Wetzel
+#' @export
+#'
+#
+combine_cs_discards <- function(
+  cs_data,
+  em_data,
+  dir = NULL) {
+
+
+
+}
