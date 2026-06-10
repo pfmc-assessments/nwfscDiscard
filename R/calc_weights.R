9bfa4085086636d972df0705940241d299974316
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2026-06-10 07:03:29 -0700

    update the processing of the alt. gemm

--- R/calc_weights.R
+++ R/calc_weights.R
@@ -9,16 +9,22 @@
 #' @param dir Directory location to save files.
 #'
 #'
-#'
 #' @author Chantel Wetzel
 #' @export
 #'
 #
-calc_weights <- function(data, include_catch_share = TRUE, dir = NULL) {
+calc_weights <- function(
+  data,
+  include_catch_share = TRUE,
+  dir = NULL
+) {
   all_areas <- unique(data$area)
   cli::cli_alert_info(
-    "The following areas are in the data: {all_areas}. Data will be grouped
-    within each area."
+    "The following areas are in the data: {all_areas}."
+  )
+  all_gears <- unique(data$gear)
+  cli::cli_alert_info(
+    "The following gears are in the data: {all_gears}."
   )
   if (length(unique(data$species)) > 1) {
     cli::cli_abort(
@@ -29,7 +35,7 @@ calc_weights <- function(data, include_catch_share = TRUE, dir = NULL) {
 
   if (include_catch_share) {
     weights <- data |>
-      dplyr::group_by(year, gear_type) |>
+      dplyr::group_by(year, area, gear_type) |>
       dplyr::mutate(
         total_discard_mt = sum(gemm_dis_est_area, na.rm = TRUE),
         total_landed_mt = sum(landings_area, na.rm = TRUE),
@@ -37,13 +43,16 @@ calc_weights <- function(data, include_catch_share = TRUE, dir = NULL) {
       ) |>
       dplyr::ungroup() |>
       dplyr::summarise(
-        .by = c("year", "catch_shares", "gear_type"),
+        .by = c("year", "area", "catch_shares", "gear_type"),
         gear_discard_mt = sum(gemm_dis_est_area, na.rm = TRUE),
         gear_landings_mt = sum(landings_area, na.rm = TRUE),
         gear_catch_mt = sum(gear_discard_mt + gear_landings_mt, na.rm = TRUE),
         prop_discard = round(gear_discard_mt / unique(total_discard_mt), 4),
         prop_landed = round(gear_landings_mt / unique(total_landed_mt), 4),
         prop_catch = round(gear_catch_mt / unique(total_catch_mt), 4)
+      ) |>
+      dplyr::mutate(
+        fleet = paste0(gear_type, "-", area)
       ) |>
       dplyr::ungroup()
   } else {
@@ -62,6 +71,7 @@ calc_weights <- function(data, include_catch_share = TRUE, dir = NULL) {
       ) |>
       dplyr::group_by(year, area, gear) |>
       dplyr::mutate(
+        fleet = paste0(gear_type, "-", area),
         prop_discard = round(
           total_discard_mt / unique(gear_group_discard_mt),
           4
--- R/format_gemm_alt.R
+++ R/format_gemm_alt.R
@@ -1,7 +1,11 @@
 #' Format GEMM alternative data to match the WCGOP
 #'
-#'
-#' @param data Data frame of the alternative GEMM data provided by the FOS
+#' @param dir Directory location to save files.
+#' @param species_name species name that should match the name in the alternative
+#'   gemm data file. Needs to be a single species.
+#' @param areas_to_keep Area names in the alternative GEMM to filter the data for.
+#' @param gears_to_keep Gear names in the alternative GEMM to filter the data for.
+#' @param data_file CSV file name of the alternative GEMM data provided by the FOS
 #'   team for calculating data weights using alternative areas. The gear type
 #'   (minor) and the gear group (major) are used to calculate totals by area
 #'   and weighting proportions.
@@ -13,8 +17,13 @@
 #'
 #
 format_gemm_alt <- function(
-  data
+  dir,
+  species_name,
+  areas_to_keep,
+  gears_to_keep,
+  data_file = "SATAN_GEMM_2026-04-12.csv"
 ) {
+  data <- read.csv(fs::path(dir, data_file))
   catch_share_sectors <- c(
     "CS - Bottom and Midwater Trawl",
     "CS - Bottom Trawl",
@@ -29,6 +38,10 @@ format_gemm_alt <- function(
     "Midwater Rockfish EM"
   )
   format_data <- data |>
+    dplyr::filter(
+      species == species_name,
+      area %in% areas_to_keep
+    ) |>
     dplyr::rename(
       gear = lilboy_fleet,
       gear_type = bigboy_fleet
@@ -46,12 +59,16 @@ format_gemm_alt <- function(
         gear == "pot" ~ "Pot",
         .default = "Shrimp Trawl"
       ),
+      gear_type = gsub("_", "-", gear_type),
       catch_shares = dplyr::case_when(
         sector %in% catch_share_sectors ~ TRUE,
         .default = FALSE
-      )
+      ),
+      landings_area = dplyr::coalesce(landings_area, 0),
+      gemm_lnd_est = dplyr::coalesce(gemm_lnd_est, 0)
     ) |>
-    dplyr::relocate(catch_shares, .after = sector)
+    dplyr::relocate(catch_shares, .after = sector) |>
+    dplyr::filter(gear %in% gears_to_keep)
 
   format_data$catch_shares[format_data$year < 2011] <- FALSE
   return(format_data)
