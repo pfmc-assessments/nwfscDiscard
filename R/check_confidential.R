2161152b6d9a863cefde11f036d8a1c936903512
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2026-06-18 10:46:30 -0700

    add back in option to do by catch shares

--- R/check_confidential.R
+++ R/check_confidential.R
@@ -1,7 +1,6 @@
 #' Check confidentiality using the catch data
 #'
 #'
-#' @param dir Directory location to save files.
 #' @param data A data frame of WCGOP catch data
 #' @param gear_groups List of gear types to group together
 #' (example: list(c("Bottom Trawl", "Midwater Trawl"), c("Hook & Line", "Pot", "Shrimp Trawl"))).
@@ -9,6 +8,9 @@
 #' @param fleet_colname Column to use to determine areas for fleets (example: "r_state.x")
 #' @param fleet_groups List of fleet groups to use (example: list(c("WA", "OR", "CA"))).
 #' @param fleet_names Vector of fleet names (example: c("coastwide")).
+#' @param dir Directory location to save files.
+#' @param  by_catch_share Logical. Calculate the number of unique vessels by catch
+#'   share for each year and fleet.
 #'
 #' @author Chantel Wetzel
 #' @export
@@ -21,15 +23,10 @@ check_confidential <- function(
   fleet_colname,
   fleet_groups,
   fleet_names,
+  by_catch_share = FALSE,
   dir = NULL
 ) {
   nwfscSurvey::check_dir(dir = dir)
-  if (sum(colnames(data) == "emtrip_id") == 1) {
-    data[, "trip_id"] <- data[, "emtrip_id"]
-    add_name <- "_em"
-  } else {
-    add_name <- ""
-  }
 
   if (sum(c("fleet_groups", "gear_groups") %in% colnames(data)) != 2) {
     if (sum(colnames(data) == "TRIP_ID") == 1) {
@@ -60,45 +57,60 @@ check_confidential <- function(
     )
   }
 
-  vessels_by_year_cs <- data |>
-    dplyr::group_by(year, gear_groups, fleet_groups, fleet, catch_shares) |>
+  if (by_catch_share) {
+    var <- c("year", "fleet", "catch_shares")
+    add_name <- "_by_catch_shares"
+  } else {
+    var <- c("year", "fleet")
+    add <- ""
+  }
+  vessels_by_year <- data |>
+    dplyr::group_by_at(var) |>
     dplyr::reframe(
       n_obs = dplyr::n(),
       n_hauls = length(unique(haul_id)),
       n_trips = length(unique(trip_id)),
       n_vessels = length(unique(drvid))
     ) |>
-    dplyr::ungroup()
+    dplyr::ungroup() |>
+    as.data.frame()
 
-  vessels_by_year_cs <- as.data.frame(vessels_by_year_cs)
-
-  if (any(vessels_by_year_cs[, "n_vessels"] < 3)) {
-    bad_fleet_group <- vessels_by_year_cs[
-      which(vessels_by_year_cs[, "n_vessels"] < 3),
+  if (any(vessels_by_year[, "n_vessels"] < 3)) {
+    bad_fleet_group <- vessels_by_year[
+      which(vessels_by_year[, "n_vessels"] < 3),
       "fleet"
     ]
-    bad_year <- vessels_by_year_cs[
-      which(vessels_by_year_cs[, "n_vessels"] < 3),
+    bad_year <- vessels_by_year[
+      which(vessels_by_year[, "n_vessels"] < 3),
       "year"
     ]
-    bad_cs <- vessels_by_year_cs[
-      which(vessels_by_year_cs[, "n_vessels"] < 3),
-      "catch_shares"
-    ]
+    if ("catch_shares" %in% colnames(vessels_by_year)) {
+      bad_cs <- paste0(
+        "catch share:",
+        vessels_by_year[
+          which(vessels_by_year[, "n_vessels"] < 3),
+          "catch_shares"
+        ]
+      )
+    } else {
+      bad_cs <- ""
+    }
     warn <- paste0(bad_year, "-", bad_fleet_group, "-", bad_cs)
-    glue::glue("The fleet grouping does not meet confidentiality for {warn}.")
+    cli::cli_alert_warning(
+      "The fleet grouping does not meet confidentiality for {warn}."
+    )
   }
 
   if (!is.null(dir)) {
     write.csv(
-      vessels_by_year_cs,
+      vessels_by_year,
       file = file.path(
         dir,
-        paste0("confidentiality", add_name, "_by_catch_share.csv")
+        paste0("wcgop_confidentiality", add_name, ".csv")
       ),
       row.names = FALSE
     )
   }
-  vessels_by_year_cs
-  return(vessels_by_year_cs)
+  vessels_by_year
+  return(vessels_by_year)
 }
